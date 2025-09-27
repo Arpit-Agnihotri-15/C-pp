038aa0888b1302ad7f13f395f7f085307aedfb31
Arpit Agnihotri <arpit151604@gmail.com>
2025-09-27 14:11:54 +0530

    Q11: Greatest among three no.s (ternary Operator).

---
+++ Q11_greatest_among_three.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;    
+int main(){
+    int a,b,c;
+    cout<<"Enter three numbers:";
+    cin>>a>>b>>c;
+    a>b?(a>c?cout<<"a is greatest":cout<<"c is greatest"):(b>c?cout<<"b is greatest":cout<<"c is greatest");
+    return 0;   
+}
