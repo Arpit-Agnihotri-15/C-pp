d985d9ee33554719ccdff001607c64c2728be3e6
Arpit Agnihotri <arpit151604@gmail.com>
2025-09-27 14:11:02 +0530

    Q10: Greatest among two no.s (ternary Operator).

---
+++ Q10_greatest_among_two.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    a>b?cout<<"a is greatest":cout<<"b is greatest";
+    return 0;
+}
