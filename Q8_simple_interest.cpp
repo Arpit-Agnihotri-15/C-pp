ac30d697051748586e77f6fe0d8559b012aeceb5
Arpit Agnihotri <arpit151604@gmail.com>
2025-09-27 14:08:13 +0530

    Q8: Simple Interest Program

---
+++ Q8_simple_interest.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main(){
+    float p,r,t;
+    cout<<"Enter the principle, rate and time:";
+    cin>>p>>r>>t;
+    cout<<"The Simple Interest is:"<<(p*r*t)/100<<endl;
+    return 0;
+}
