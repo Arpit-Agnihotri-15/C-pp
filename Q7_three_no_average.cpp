15161205d3ee88e4a7e1876e9eb1458d4b6f3afb
Arpit Agnihotri <arpit151604@gmail.com>
2025-09-27 14:07:00 +0530

    Q7: Average of three no.s Program

---
+++ Q7_three_no_average.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main() {
+    float a, b, c;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+    cout << "The Average is: " << (a + b + c) / 3 << endl;
+    return 0;
+}
