9889e067816c24b0b4d499623e64506aa8a1baa2
Arpit Agnihotri <arpit151604@gmail.com>
2025-09-27 14:09:11 +0530

    Q9: Swap two no.s without using 3rd var.

---
+++ Q9_swap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter First number:";
+    cin>>a;
+    cout<<"Enter second number:";
+    cin>>b;
+    cout<<"\nBefore Swapping: a ="<<a<<", b="<<b<<endl;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"\nAfter Swapping: a ="<<a<<", b="<<b<<endl;      
+    return 0;
+}
