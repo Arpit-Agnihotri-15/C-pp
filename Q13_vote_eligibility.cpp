fb202edd1e9ff630a580bdf702df31620cceb330
Arpit Agnihotri <arpit151604@gmail.com>
2025-09-28 08:40:56 +0530

    Delete Q13_vote_eligibility.cpp

--- Q13_vote_eligibility.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream>
-using namespace std;
-int main(){
-    int age;
-    cout << " Enter your age:";
-    cin >> age;
-    age >=18 ? cout << " You are eligible to vote." :cout << " You are not eligible to vote.";
-    return 0;
-}
